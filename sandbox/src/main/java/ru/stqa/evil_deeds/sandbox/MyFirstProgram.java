ece527c68c4b8e4eef118a2b42ed58f11820b654
griffinheart <morginal@protonmail.com>
2017-11-30 17:04:05 +0300

    few functions added

--- sandbox/src/main/java/ru/stqa/evil_deeds/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/evil_deeds/sandbox/MyFirstProgram.java
@@ -4,6 +4,30 @@ public class MyFirstProgram
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Alexei");
+
+        double l = 5;
+        System.out.println("Square's area with side length " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Rectangle's area with sides lengths " + a + " and " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody)
+    {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len)
+    {
+        return len * len;
+    }
+
+    public static double area(double a, double b)
+    {
+        return a * b;
     }
 }
