36739e066d8da8f48f47c2ca578fda5a086decbe
griffinheart <morginal@protonmail.com>
2017-12-01 07:49:22 +0300

    Few more tests added for Point class

--- sandbox/src/test/java/ru/stqa/evil_deeds/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/evil_deeds/sandbox/PointTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.evil_deeds.sandbox;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,5 +16,11 @@ public class PointTests
         Point p3 = new Point(1, 2);
         Point p4 = new Point(7, 9);
         Assert.assertEquals(p1.distance(p3, p4), 9.219544457292887);
+
+        Point p5 = new Point(4, 6);
+        Point p6 = new Point(2, 3);
+
+        Assert.assertFalse(Boolean.valueOf(String.valueOf(p2.distance(p5, p6))));
+        Assert.assertNotEquals(p1.distance(p5, p6), 3.605551275463980);
     }
 }
