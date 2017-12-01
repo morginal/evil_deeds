87b5fbba24be81e85b42022dba28739c185d2eb3
griffinheart <morginal@protonmail.com>
2017-12-01 22:02:15 +0300

    changes added to task 3

--- sandbox/src/test/java/ru/stqa/evil_deeds/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/evil_deeds/sandbox/PointTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.evil_deeds.sandbox;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,16 +10,11 @@ public class PointTests
     {
         Point p1 = new Point(4, 6);
         Point p2 = new Point(2, 3);
-        Assert.assertEquals(p1.distance(p1, p2), 3.605551275463989);
+        Point p3 = new Point(7, 9);
 
-        Point p3 = new Point(1, 2);
-        Point p4 = new Point(7, 9);
-        Assert.assertEquals(p1.distance(p3, p4), 9.219544457292887);
-
-        Point p5 = new Point(4, 6);
-        Point p6 = new Point(2, 3);
-
-        Assert.assertFalse(Boolean.valueOf(String.valueOf(p2.distance(p5, p6))));
-        Assert.assertNotEquals(p1.distance(p5, p6), 3.605551275463980);
+        Assert.assertEquals(p1.distance(p2), 3.605551275463989);
+        Assert.assertEquals(p1.distance(p3), 4.242640687119285);
+        Assert.assertNotEquals(p1.distance(p2), 3.605551275463980);
+        Assert.assertFalse(Boolean.valueOf(String.valueOf(p2.distance(p2))));
     }
 }
